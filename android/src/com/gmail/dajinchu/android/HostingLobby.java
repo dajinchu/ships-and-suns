fb5df2540a40f4b19c6f4961334cf23e9d11f41a
Da-Jin Chu <dajinchu@gmail.com>
2015-01-30 22:09:27 -0500

    Revert "[BAD]Tried switcching HostingLobby and LANConnect to functionalServer branch"
    This reverts commit 7501d1f45694b947f3d8f52b67c115270cc2c97b.

--- android/src/com/gmail/dajinchu/android/HostingLobby.java
+++ android/src/com/gmail/dajinchu/android/HostingLobby.java
@@ -1,14 +1,32 @@
 package com.gmail.dajinchu.android;
 
 import android.os.AsyncTask;
-import android.util.Log;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Stack;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.ui.VerticalGroup;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.utils.TimeUtils;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.gmail.dajinchu.MainGame;
 import com.gmail.dajinchu.Model;
 import com.gmail.dajinchu.net.SocketServerManager;
+import com.splunk.mint.Mint;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -27,6 +45,7 @@ import javax.jmdns.ServiceInfo;
 public class HostingLobby implements Screen{
 
     private final MainGame mainGame;
+    private String name;
     private ServerSocket serverSocket;
     String TAG = "HostingLobby";
     private Socket client;
@@ -34,9 +53,68 @@ public class HostingLobby implements Screen{
     private ServiceInfo serviceInfo;
     private Model model;
 
-    public HostingLobby(MainGame mainGame, JmDNS jmdns){
+    private SpriteBatch spriteBatch = new SpriteBatch();
+    private final ShapeRenderer shapeRenderer;
+
+    //Scene2d set-up
+    private Skin skin;
+    private Stage stage;
+
+    //UI
+    private final VerticalGroup playerList;
+    private Table table;
+    private final ImageButton go;
+
+    public HostingLobby(MainGame mainGame, JmDNS jmdns, String name){
         this.mainGame = mainGame;
         this.jmdns = jmdns;
+        this.name = name;
+
+        shapeRenderer = new ShapeRenderer();
+        shapeRenderer.setAutoShapeType(true);
+
+        TextureAtlas atlas = new TextureAtlas("game.pack");
+        skin = new Skin(Gdx.files.internal("uiskin.json"));
+        skin.addRegions(atlas);
+
+        stage = new Stage(new ExtendViewport(640,480));
+        Gdx.input.setInputProcessor(stage);
+
+        //Player List
+        playerList = new VerticalGroup();
+        playerList.setDebug(true);
+        playerList.space(10);
+        playerList.addActor(new Label("Participants", skin));
+        playerList.fill();
+        addParticipant("Da-Jin Chu");
+        addParticipant("Bob");
+        ScrollPane players = new ScrollPane(playerList);
+        players.setDebug(true);
+        //GO Button, adding clickListener later, AFTER we have connected with someone
+        go = new ImageButton(skin.getDrawable("play"),skin.getDrawable("play_down"));
+        //Map selector
+        Drawable mapimg = skin.getDrawable("map1");
+        Image map = new Image(mapimg);
+        //Right Pane:
+        Table rightPane = new Table();
+        rightPane.setDebug(true);
+        rightPane.add(new Label("<",skin));
+        rightPane.add(map).height(300).width(300).pad(10);
+        rightPane.add(new Label(">", skin));
+        rightPane.row();
+        rightPane.add(go).colspan(3).width(200).height(75).expandY().right();
+
+        TextField nameText = new TextField("", skin);
+        Label addressLabel = new Label("Address:", skin);
+        TextField addressText = new TextField("", skin);
+
+        table = new Table();
+        table.setFillParent(true);
+        table.setDebug(true);
+        stage.addActor(table);
+        table.add(players).expand().top().fillX().pad(10);
+        table.add(rightPane).fill();
+
         new ASyncConnect().execute();
     }
 
@@ -45,21 +123,26 @@ public class HostingLobby implements Screen{
         @Override
         protected Void doInBackground(Void... voids) {
             try {
-                serviceInfo = ServiceInfo.create("_ships._tcp.local.", "AndroidTest", 0, "plain test service from android");
+                serviceInfo = ServiceInfo.create("_ships._tcp.local.", name, 13079, "plain test service from android");
                 //notifyUser("This IP: " + deviceIpAddress);
                 jmdns.registerService(serviceInfo);
                 if (serverSocket == null) {
                     serverSocket = new ServerSocket(13079);//Random hardcoded port
-                    Log.i(TAG, "Server socket opened");
+                    Mint.leaveBreadcrumb("Server socket opened");
                 }
+                Mint.leaveBreadcrumb("Server socket="+serverSocket.toString());
                 client = serverSocket.accept();
-                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
-                BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+                final BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
                 // Write output
                 sendInitalSetup(writer);
-                sendStart(writer, br);
-
-                //writer.close();
+                //AFTER sending inital setup info, we can "activate" the go button to have actions
+                go.addListener(new ClickListener(){
+                    @Override
+                    public void clicked(InputEvent event, float x, float y){
+                        sendStart(br,writer);
+                    }
+                });
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -68,14 +151,14 @@ public class HostingLobby implements Screen{
         }
     }
 
-    public void sendStart(final BufferedWriter writer, final BufferedReader reader){
+    public void sendStart(final BufferedReader reader, final BufferedWriter writer){
         try {
             writer.write("Start\n");
             Gdx.app.log("HostingLobby", "sent start");
             Gdx.app.postRunnable(new Runnable() {
                 @Override
                 public void run() {
-                    mainGame.startGame(model, new SocketServerManager(reader,writer));
+                    mainGame.startGame(model, new SocketServerManager(reader, writer));
                 }
             });
             writer.flush();
@@ -98,19 +181,29 @@ public class HostingLobby implements Screen{
         }
     }
 
+
+    public void addParticipant(String name){
+        Stack participant = new Stack();
+        participant.add(new Image(skin.getPatch("button")));
+        participant.add(new Label(name, skin));
+        playerList.addActor(participant);
+    }
+
     @Override
     public void render(float delta) {
-
+        Gdx.gl.glClearColor(255,255,255,0);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
     }
 
     @Override
     public void resize(int width, int height) {
-
+        stage.getViewport().update(width, height, true);
     }
 
     @Override
     public void show() {
-
     }
 
     @Override
@@ -130,6 +223,7 @@ public class HostingLobby implements Screen{
 
     @Override
     public void dispose() {
-
+        stage.dispose();
+        shapeRenderer.dispose();
     }
 }
