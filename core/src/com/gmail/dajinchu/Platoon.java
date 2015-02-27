0f2f1fab8904b2c1d4ab894f039113b7a7d27eb8
Da-Jin Chu <dajinchu@gmail.com>
2015-02-27 15:53:25 -0500

    Added Platoon class: group of ships with an objective. Stops accidental capture

---
+++ core/src/com/gmail/dajinchu/Platoon.java
@@ -0,0 +1,13 @@
+package com.gmail.dajinchu;
+
+/**
+ * Created by Da-Jin on 2/27/2015.
+ */
+//A platoon represents a group of ships with an objective.
+public class Platoon {
+    //Array<Ship> members = new Array<Ship>();
+
+    public Platoon(){
+
+    }
+}
